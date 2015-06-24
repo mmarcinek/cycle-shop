6b9016cf9dbb2915cb1c4e4745490743f7ee4753
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-24 16:59:26 -0400

    Saves progress

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,24 @@
 
   'use strict';
 
-  angular.module('CycleShop', []);
+  angular.module('CycleShop', ['ngRoute'])
+
+  .config(['$routeProvider',
+    function( $routeProvider){
+
+      $routeProvider.when('/', {
+        controller: 'Cycles',
+        templateUrl: 'js/cycle/home.tpl.html'
+      })
+
+      .when('/add', {
+        controller: 'Cycles',
+        templateUrl: 'js/cycle/addshop.tpl.html'
+      });
+    }
+
+
+    ]);
+
 
 }());
