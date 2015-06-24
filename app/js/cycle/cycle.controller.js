46363bd038be30a56976c7499fe990e6e2d0477a
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-24 17:52:32 -0400

    Sets up Parse back-end

--- app/js/cycle/cycle.controller.js
+++ app/js/cycle/cycle.controller.js
@@ -4,9 +4,30 @@
 
   angular.module('CycleShop')
 
-  .controller('Cycles', ['$scope', function ($scope)
+  .controller('Cycles', ['$scope', '$http', 'PARSE', '$location',
 
+  function ($scope, $scope, $http, PARSE, $location) {
 
+    var Shop = function (options) {
+      this.name = options.name;
+      this.address = options.address;
+      this.phone = options.phone;
+      this.description = options.description;
+    };
+
+    var addShop = function (x){
+      var store = new Shop(x);
+
+      $http.post('PARSE', 'classes/shops', store, PARSE.CONFIG)
+
+      success( function (){
+        $location.path('/');
+        $scope.store = {};
+
+      });
+
+    };
+  }
 
   ])
 
--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,16 @@
 
   angular.module('CycleShop', ['ngRoute'])
 
+  .constant('PARSE', {
+    URL: 'https://api.parse.com/1/',
+    CONFIG: {
+      headers: {
+        'X-Parse-Application-Id' : 'BmPAHUNCZHUN4bNhwWKaCgz4rEur01PGPUDGL5aT',
+        'X-Parse-REST-API-Key'  : 'Ubl7DZKMhYWZUko0fDwqjSklQ5x7J2RCWoYZNIbc'
+      }
+    }
+  })
+
   .config(['$routeProvider',
     function( $routeProvider){
 
